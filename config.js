41430d817b1a88ca68a8687f367de243437d0664
gjiki <jikia.gegi@gmail.com>
2020-04-29 14:29:38 +0400

    Minor front upgrade
    Little design upgrade for board, body, buttons and textbox.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const INTERVAL = 200;
 export const SNAKE_DEFAULT_LENGTH = 5;
 export const SNAKE_CUBE_DIM = 10;
-export const BOARD_SIZE = 320;
+export const BOARD_SIZE = 420;
 export const BOARD_BORDER = 10;
